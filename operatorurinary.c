2c7784fcf69c12e9afd2c5bf0571d95fbcf12cbd
sanju <sanjupriya6019@gmail.com>
2026-06-08 08:19:44 +0530

    Add operatorurinary post increment

---
+++ operatorurinary.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int x=10,y=12;
+    y=x++;
+    printf("the value of x is %d\n",x);
+    printf("the valuve of y is %d\n",y);
+    return 0;
+
+}
