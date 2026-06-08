70166d9eb44a4bf6226ea20e61ecb0e9101cbcfa
sanju <sanjupriya6019@gmail.com>
2026-06-08 23:11:35 +0530

    Add operatorurinaryprifix

---
+++ operatorurinaryprifix.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int x=10,y=20;
+    y=++x;
+    printf("the value of x is %d\n",x);
+    printf("the value of y is %d\n",y);
+    return 0;
+
+}
