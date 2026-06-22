ed8aee19849decfe9e58a996890787b6ad6abf88
sanju <sanjupriya6019@gmail.com>
2026-06-22 14:05:47 +0530

    Add prime_number

--- prime_number.c
+++ prime_number.c
@@ -1,23 +1,34 @@
-#include<stdio.h>
+/*
+Algorithm:
+start
+Read a number from the user.
+If the number is less than or equal to 1, it is not prime.
+If divisible by any number , it is not prime number.
+Otherwise, it is a prime number .
+Print the result.
+stop.
+*/
+#include <stdio.h>
 int main(){
     int num,i,flag=1;
-    printf("enter a number:");
-    scanf("%d",&num);
-    if (num<=1){
-        flag=0;
-    }
-    else {
-        for (i=2;i<num;i++){
-            if (num %i==0){
-                flag =0;
-                break;
-            }
+printf("enter the number:");
+scanf("%d",&num);
+if (num<=1){
+    flag=0;
+}
+else{
+    for(i=2;i<num;i++){
+        if (num%i==0){
+            flag=0;
+            break;
+
         }
     }
-    if (flag==1)
-        printf("%d is a prime number\n",num);
-        else 
-        printf("%d is not a prime number",num);
-    return 0;
-    
+}
+  if(flag==1)
+       printf("%d is a prime number\n",num);
+  else
+      printf("%d is not a prime number\n",num);
+  return 0;
+
 }
