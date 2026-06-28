c0688903ed65d055182a2e64137e153847926e1a
sanju <sanjupriya6019@gmail.com>
2026-06-28 07:08:26 +0530

    Add Reverse string pointer

---
+++ Reverse_string_pointer.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str [100];
+    printf("enter a string:");
+    scanf("%s",str);
+    char temp;
+    char *start = str;
+    char *end = str + strlen(str) - 1;
+    while(start < end){
+        temp=*start;
+        *start=*end;
+        *end = temp;
+        start ++;
+        end --;
+    }
+printf("Reverse string is: %s \n",str);
+    return 0;
+}
